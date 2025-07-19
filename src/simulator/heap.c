7203acebb639e3af0eef9c9b3c27a76d4e792849
Will <wpunter2102@gmail.com>
2025-07-19 14:00:23 +0100

    feat: added helper functions for obtaining parent and child indices of node in heap.

--- src/simulator/heap.c
+++ src/simulator/heap.c
@@ -17,6 +17,9 @@ struct heap {
 };
 
 /*  Forward declare helper functions. */
+static inline unsigned int heap_get_parent_index(unsigned int index);
+static inline unsigned int heap_get_left_child_index(unsigned int index);
+static inline unsigned int heap_get_right_child_index(unsigned int index);
 
 /*  Heap API implementation. */
 
@@ -54,11 +57,167 @@ void heap_free(heap_t heap) {
     }
 
     free((void *) heap);
-}
+};
 
-void heap_insert(heap_t, void *);
+/*  Insert a new element into a heap - a heap must always take the form of a 
+    complete binary tree (where all layers but the last are full, and the last
+    layer has its elements fill in from the left side) so to maintain this
+    structure, we insert the new element at the end of the array (reallocating
+    if / as necessary) and then bubble the new element up until it finds a
+    place which preserves the heap property. This works because if the new node
+    is smaller than its parent, then it will also be smaller than all of the
+    parents' children. Similarly, the parent is smaller than all of its
+    childrens' children so swapping the two will not affect the heap
+    property. */
+void heap_insert(heap_t heap, void *elem) {
+    assert(heap);
+    assert(elem);
+    assert(heap->elems);
+    assert(heap->comparator);
+
+    /*  Resize if capacity exceeded. */
+    if (heap->size + 1 > heap->capacity) {
+        heap->capacity = heap->capacity * 2;
+        heap->elems = (void **) realloc(sizeof(void *) * heap->capacity);
+    }
+
+    heap->elems[heap->size] = elem;
+    heap->size += 1;
+
+    unsigned int index = heap->size - 1;
+    unsigned int parent_index;
+    //while (index != 0 && heap->comparator(heap->elems[index]), ) {
+
+    //}
+};
+
 void *heap_min(heap_t);
 void *heap_pop_min(heap_t);
 unsigned int heap_size();
 
 /*  Helper function implementations. */
+
+/*  Get parent index:
+    Note that in the following we work with a 1 index for our tree and the
+    underyling array.
+
+    As a visual aid, the following is a binary tree with nodes labelled as
+    indices.
+
+        Array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        Complete binary tree:
+        row | Tree
+        1   |           1
+            |          / \
+            |         /   \
+            |        /     \
+        2   |       2       3
+            |      / \     / \
+            |     /   \   /   \
+        3   |    4     5 6     7
+            |   / \   /
+        4   |  8   9 10
+
+        Then the offset for each element on a given row is the position from
+        the leftmost element on that row.
+
+    Proof:
+    
+    We first assume / define the following relations based on the structure
+    of a complete binary tree:
+    
+    row(x) = floor(log_2(x)) + 1
+    offset(x) = x - 2^(row(x) - 1) + 1
+              = x - 2^(floor(log_2(x)) + 1 - 1) + 1
+              = x - 2^(floor(log_2(x))) + 1
+    
+    parent(x, y) = row(y) = row(x) + 1 and
+                   (offset(y) = 2 * offset(x) - 1 or
+                    offset(y) = 2 * offset(x))
+    
+    Some examples being:
+        row(1) = floor(log_2(1)) + 1 = floor(0) + 1 = 1 
+        
+        row(6) = floor(log_2(6)) + 1 = floor(2.584962501) + 1 = 2 + 1 = 3
+
+        offset(1) = 1 - 2^(floor(log_2(1))) + 1 = 1 - 2^(floor(0)) + 1 =
+            1 - 2^0 + 1 = 1 - 1 + 1 = 0 + 1 = 1
+        
+        offset(6) = 6 - 2^(floor(log_2(6))) + 1 =
+            6 - 2^(floor(2.584962501)) + 1 =
+            6 - 2^(2) + 1 = 6 - 4 + 1 = 2 + 1 = 3
+            
+    Now we will prove that parent(x,y) -> y = 2x or y = 2x + 1.
+    
+    Assume parent(x, y). Then by the definition of the relation:
+        (1) row(y) = row(x) + 1
+        (2) offset(y) = 2 * offset(x) - 1 or
+            offset(y) = 2 * offset(x)
+    
+    Expanding these based on their numerical definition:
+        (1a) floor(log_2(y)) + 1 = floor(log_2(x)) + 1 + 1 [by def. row]
+        (1b) floor(log_2(y)) = floor(log_2(x)) + 1         [by arithmetic]
+
+        (2a) y - 2^(floor(log_2(y))) + 1 = 2(x - 2^(floor(log_2(x))) + 1) - 1
+             or
+             y - 2^(floor(log_2(y))) + 1 = 2(x - 2^(floor(log_2(x))) + 1)
+             [by def. offset]
+
+    Next, we substitute (1b) into (2a):
+        (2b) y - 2^(floor(log_2(x)) + 1) + 1 =
+                 2(x - 2^(floor(log_2(x))) + 1) - 1
+            or
+             y - 2^(floor(log_2(x)) + 1) + 1 =
+                 2(x - 2^(floor(log_2(x))) + 1)
+            [by 1b]
+        
+    And convert the 2^(f(x) + 1) to 2 * 2^(f(x)) by index laws.
+        (2c) y - 2 * 2^(floor(log_2(x))) + 1 =
+                 2(x - 2^(floor(log_2(x))) + 1) - 1
+            or
+             y - 2 * 2^(floor(log_2(x))) + 1 =
+                 2(x - 2^(floor(log_2(x))) + 1)
+            [by aritmetic]
+    
+    Then expand the brackets on the RHS.
+        (2d) y - 2 * 2^(floor(log_2(x))) + 1 =
+                 2x - 2 * 2^(floor(log_2(x))) + 2 - 1
+            or
+             y - 2 * 2^(floor(log_2(x))) + 1 =
+                 2x - 2 * 2^(floor(log_2(x))) + 2
+            [by aritmetic]
+    
+    Then add 2 * 2^(floor(log_2(x))) to both sides.
+        (2e) y + 1 = 2x + 2 - 1 or
+             y + 1 = 2x + 2
+            [by aritmetic]
+    
+    Finally, simplifying gives:
+        (2f) y = 2x     or
+             y = 2x + 1
+            [by aritmetic]
+
+    as required.
+
+    Q.E.D.
+
+    Modifying this for 0 indexed values is easy - we just subtract one from
+    the indices.
+
+    Suppose in 1-index: x has children y = 2x or y = 2x + 1
+    Then in 0-index: x - 1 has children y = 2x - 1 or y = 2x
+    So if i = x - 1, then x = i + 1 and then i has children j = 2x - 1 or j = 2x
+    so j = 2(i + 1) - 1 = 2i + 1 or j = 2(i + 1) = 2i + 2.
+    So when 0-indexed, i has children j = 2i + 1 and 2i + 2. */
+static inline unsigned int heap_get_parent_index(unsigned int index) {
+    return index / 2;
+};
+
+static inline unsigned int heap_get_left_child_index(unsigned int index) {
+    return 2 * index + 1;
+}
+
+static inline unsigned int heap_get_right_child_index(unsigned int index) {
+    return 2 * index + 2;
+}
