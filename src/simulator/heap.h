0c32a520d96ec7a07a465c19e9a849c956bc3b70
Will <wpunter2102@gmail.com>
2025-07-19 11:11:05 +0100

    feat: first commit

---
+++ src/simulator/heap.h
@@ -0,0 +1,12 @@
+/*  heap.h
+
+    Generic array-based heap. */
+
+#ifndef HEAP_H
+
+/*  Forward declare heap in this file but define it in .c file as heap
+    implementation is hidden to users for modularity. */
+struct heap;
+typedef struct heap *heap_t;
+
+#endif
