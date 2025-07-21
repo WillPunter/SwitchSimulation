388648a1c63d739f5a7798e47451a46f8fb91ca8
Will <wpunter2102@gmail.com>
2025-07-21 13:55:58 +0100

    feat: implemented simulator main loop

--- src/simulator/simulator.c
+++ src/simulator/simulator.c
@@ -34,13 +34,16 @@ struct double_time {
 typedef struct double_time *double_time_t;
 
 typedef void (*add_func_t)(void *, void *);
+typedef void (*copy_func_t)(void *, void *);
 
 static event_table_t event_table;
 static heap_t event_queue;
 static add_func_t add_time;
 static comparator_func_t compare_time;
+static copy_func_t copy_time;
 static free_func_t free_time;
 static void *current_time;
+static char should_terminate;
 
 /*  Forward declare helper functions. */
 static comparison_t compare_event(void *lhs, void *rhs);
@@ -48,10 +51,12 @@ static void free_event(void *event);
 static uint_time_t create_uint_time(unsigned int time);
 static void add_uint_time(void *lhs, void *rhs);
 static comparison_t compare_uint_time(void *lhs, void *rhs);
+static void copy_uint_time(void *lhs, void *rhs);
 static void free_uint_time(void *uint_time);
 static double_time_t create_double_time(double time);
 static void add_double_time(void *lhs, void *rhs);
 static comparison_t compare_double_time(void *lhs, void *rhs);
+static void copy_double_time(void *lhs, void *rhs);
 static void free_double_time(void *double_time);
 
 /*  Simulator API implementation. */
@@ -72,15 +77,19 @@ void simulator_init(
     time_type_t time_type,
     add_func_t custom_time_add,
     comparator_func_t custom_time_compare,
+    copy_func_t custom_time_copy,
     free_func_t custom_time_free,
     void *start_time
 ) {
+    should_terminate = 0;
+
     event_table = event_table_create();
 
     switch (time_type) {
         case UINT_TIME: {
             add_time = add_uint_time;
             compare_time = compare_uint_time;
+            copy_time = copy_uint_time;
             free_time = free_uint_time;
             current_time = create_uint_time(0);
             break;
@@ -89,6 +98,7 @@ void simulator_init(
         case DOUBLE_TIME: {
             add_time = add_double_time;
             compare_time = compare_double_time;
+            copy_time = copy_double_time;
             free_time = free_double_time;
             current_time = create_double_time(0.0);
             break;
@@ -100,6 +110,7 @@ void simulator_init(
             assert(start_time);
             add_time = custom_time_add;
             compare_time = custom_time_compare;
+            copy_time = custom_time_copy;
             free_time = custom_time_free;
             current_time = start_time;
             break;
@@ -151,6 +162,36 @@ void simulator_invoke_event(event_id_t evt_id, void *arg, void *future_time) {
 };
 
 /*  Simulator main loop. */
+void simulator_main_loop() {
+    while (!should_terminate && heap_size(event_queue) > 0) {
+        /*  Dequeue next event and advance current time. */
+        void *next_event_ptr = heap_pop_min(event_queue);
+        assert(next_event_ptr);
+
+        event_t next_event = (event_t) next_event_ptr;
+        time_copy(current_time, next_event->time);
+
+        /* Lookup event in event table and execute callback with arg. */
+        callback_func_t callback;
+        free_func_t free_arg;
+        
+        event_table_lookup(
+            event_table,
+            next_event->evt_id,
+            &callback,
+            &free_arg
+        );
+
+        if (callback && free_arg) {
+            callback(next_event->arg);
+            free_arg(next_event->arg);
+        };
+
+        /*  Free time and event structures now dequeued. */
+        free_time(next_event->time);
+        free(next_event_ptr);
+    };
+};
 
 /*  Helper function implementations. */
 
@@ -232,6 +273,17 @@ static comparison_t compare_uint_time(void *lhs, void *rhs) {
     };
 };
 
+/*  Copy unit time - copy time value on lhs into rhs. */
+static void copy_uint_time(void *lhs, void *rhs) {
+    assert(lhs);
+    assert(rhs);
+
+    uint_time_t time_lhs = (uint_time_t) lhs;
+    uint_time_t time_rhs = (uint_time_t) rhs;
+
+    time_rhs->time = time_lhs->time;
+};
+
 /*  Free uint time - simply frees the provided uint_time structure. */
 static void free_uint_time(void *uint_time) {
     assert(uint_time);
@@ -279,6 +331,17 @@ static comparison_t compare_double_time(void *lhs, void *rhs) {
         return EQ;
     };
 };
+
+/*  Copy double time - copy the lhs time value into the rhs. */
+static void copy_double_time(void *lhs, void *rhs) {
+    assert(lhs);
+    assert(rhs);
+
+    double_time_t time_lhs = (double_time_t) lhs;
+    double_time_t time_rhs = (double_time_t) rhs;
+
+    time_rhs->time = time_lhs->time;
+}
 
 /*  Free double time - simply free the provided double time structure. */
 static void free_double_time(void *double_time) {
