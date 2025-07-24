d2a36cf84e6361ddc202b496e888577c29b47a9d
Will <wpunter2102@gmail.com>
2025-07-24 12:56:28 +0100

    feat: implemented generic and cycle simulation switch interfaces.

--- src/network_switch/network_switch.h
+++
@@ -1,7 +0,0 @@
-/*  network_switch.h
-
-    Generic switch interface for any switch architecture.
-    
-    This is possible because all switches fundamentally do the same thing, i.e.
-    receiving packets through input ports and outputting them to the
-    corresponding output ports. */
---
+++ src/network_switch/switch_common.h
@@ -0,0 +1,13 @@
+/*  switch_common.h
+
+    Common switch-relevant definitions. */
+
+#ifndef SWITCH_COMMON_H
+#define SWITCH_COMMON_H
+
+const int PACKET_SIZE = 64;
+typedef void *packet_t;
+
+typedef unsigned int port_num_t;
+
+#endif
